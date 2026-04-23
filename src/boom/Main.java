cc015140b5adba7b42dc2ce1796bf119e2ea140e
king90402 <alejosalgado531@gmail.com>
2026-04-23 16:26:29 -0500

    Carga Incial del Proyecto

---
+++ src/boom/Main.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package boom;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author alejo
+ */
+
+    
+    public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("vista.fxml"));
+        Scene scene = new Scene(loader.load());
+        stage.setScene(scene);
+        stage.setTitle("Mi App JavaFX");
+        stage.show();
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
+    
+

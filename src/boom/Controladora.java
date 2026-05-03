6a87969fdf8547936bd5e1d9974baee687c0aa65
Jd-Alvarado <josedaniel0945274527@gmail.com>
2026-05-02 22:54:46 -0500

    - Creacion e inicialización de las listas correspondientes a Admins, Clientes y Productos. -Creacion de metodos dedicados al manejo de la lista de productos

--- src/boom/Controladora.java
+++ src/boom/Controladora.java
@@ -9,5 +9,109 @@ package boom;
  * @author alejo
  */
 public class Controladora {
-    // Clientes-, Admins-, Productos-
+    private static final int mx = 500;
+    
+    private producto[] listaProductos;
+    private cliente[] listaClientes;
+    private admin[] listaAdmins;
+
+    private int contadorProductos;
+    private int contadorClientes;
+    private int contadorAdmins;
+
+    public Controladora() {
+        listaProductos = new producto[mx];
+        listaClientes = new cliente[mx];
+        listaAdmins = new admin[mx];
+        
+        contadorProductos = 0;
+        contadorClientes = 0;
+        contadorAdmins = 0;
+    }
+    
+// Metodos correspondientes a la lista de productos 
+    
+    // Agregar un producto
+    public boolean agregarProducto(producto producto) {
+        if (contadorProductos < mx) {
+            listaProductos[contadorProductos] = producto;
+            contadorProductos++;
+            return true;
+        }
+        return false;
+    }
+    
+    // Eliminar un producto por su posicion 
+    public boolean eliminarProducto(int pos) {
+        if (pos >= 0 && pos < contadorProductos) {
+            for (int i = pos; i < contadorProductos - 1; i++) {
+                listaProductos[i] = listaProductos[i + 1];
+            }
+            listaProductos[contadorProductos - 1] = null;
+            contadorProductos--;
+            return true;
+        }
+        return false;
+    }
+    
+    // Eliminar un producto por su ID
+    public boolean eliminarProductoPorId(String IdProducto) {
+        for (int i = 0; i < contadorProductos; i++) {
+            if (listaProductos[i].getIdProducto().equals(IdProducto)) {
+                return eliminarProducto(i);
+            }
+        }
+        return false;
+    }
+    
+    // Buscar un producto por su ID
+    public producto buscarProductoPorId(String IdProducto) {
+        for (int i = 0; i < contadorProductos; i++) {
+            if (listaProductos[i].getIdProducto().equals(IdProducto)) {
+                return listaProductos[i];
+            }
+        }
+        return null;
+    }
+    
+    // Buscar productos por nombre (puede haber varios)
+    public producto[] buscarProductosPorNombre(String NombreProducto) {
+        producto[] resultados = new producto[contadorProductos];
+        int contador = 0;
+        for (int i = 0; i < contadorProductos; i++) {
+            if (listaProductos[i].getNombreProducto().toLowerCase().contains(NombreProducto.toLowerCase())) {
+                resultados[contador] = listaProductos[i];
+                contador++;
+            }
+        }
+        // Crear array del tamaño exacto
+        producto[] resultadosFinales = new producto[contador];
+        for (int i = 0; i < contador; i++) {
+            resultadosFinales[i] = resultados[i];
+        }
+        return resultadosFinales;
+    }
+    
+    // Buscar productos por categoría
+    public producto[] buscarProductosPorCategoria(String CategoriaProducto) {
+        producto[] resultados = new producto[contadorProductos];
+        int contador = 0;
+        for (int i = 0; i < contadorProductos; i++) {
+            if (listaProductos[i].getCategoriaProducto().equalsIgnoreCase(CategoriaProducto)) {
+                resultados[contador] = listaProductos[i];
+                contador++;
+            }
+        }
+        producto[] resultadosFinales = new producto[contador];
+        for (int i = 0; i < contador; i++) {
+            resultadosFinales[i] = resultados[i];
+        }
+        return resultadosFinales;
+    }
+    
+    // Obtener la cantidad de productos
+    public int getCantidadProductos() {
+        return contadorProductos;
+    }
+
 }
